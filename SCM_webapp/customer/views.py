21e7aebe0457624ad9efe4ca6d1fe5ac462296c4
Fazil Metharsha Y <fazilmetharsha@gmail.com>
2025-04-03 00:11:16 +0530

    Update views.py

--- SCM_webapp/customer/views.py
+++ SCM_webapp/customer/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def customer_home(request):
     if request.method=='POST':
-        return render(request,'customer_home.html',{})
+        return render(request,'customer_home.html',{'username': request.user.username})
     else:
-        return render(request,'customer_home.html',{})
+        return render(request,'customer_home.html',{'username': request.user.username})
