4d215a0c2432be0d039194fa1b446a0ad104479a
Fazil Metharsha Y <fazilmetharsha@gmail.com>
2025-04-04 19:01:13 +0530

    m

--- roles.sql
+++ roles.sql
@@ -1,1 +1,19 @@
-create role manager with login password 'manager';
+--1)
+drop owned by customer;
+drop role customer;
+create role customer with login password 'customer_pass';
+
+grant select on cus_shipments to customer;
+grant select on customers to customer;
+
+--set role customer;
+--select * from get_orders('Alice Johnson');
+--set role postgres;
+
+
+--2)
+drop owned by manager;
+drop role manager;
+create role manager with login password 'manager_pass';
+
+
--- views.sql
+++ views.sql
@@ -4,12 +4,12 @@ drop view if exists cus_shipments;
 
 create or replace view  cus_shipments as 
 (
-with curr_shipment as (select distinct shipping_id, first_value (curr_warehouse) over w as curr_warehouse,
-first_value (next_warehouse) over w as next_warehouse from shipping_details 
-window w as (partition by shipping_id order by detail_id desc))
-	
-select cs.shipping_id, curr_warehouse, next_warehouse from curr_shipment as cs,shippings as s 
-where  s.shipping_status = 'shipped' and s.shipping_id=cs.shipping_id
+	select o.customer_id ,shp.tracking_number, p.name, sup.name as sup_name, od.quantity, od.amount, shp.shipping_status
+	from products as p, suppliers as sup, order_details as od, orders as o , inventory as i,
+	shippings as shp where o.order_id = od.order_id and 
+	od.shipping_id = shp.shipping_id and od.inventory_id = i.inventory_id and 
+	i.product_id= p.product_id and i.supplier_id = sup.supplier_id
 );
 
 select * from cus_shipments;
+
