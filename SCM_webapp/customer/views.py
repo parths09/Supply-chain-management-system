a1dd81df013d8ced3ef1e2300c242b6905d03fbe
Fazil Metharsha Y <fazilmetharsha@gmail.com>
2025-04-05 01:52:05 +0530

    m

--- SCM_webapp/customer/views.py
+++ SCM_webapp/customer/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from customer.query import *
+from django.contrib import messages
 
 # Create your views here.
 def customer_home(request):
@@ -10,15 +11,25 @@ def customer_home(request):
     
 
 def customer_shipments(request):
-    # Example data (replace with real data from DB)
+
     shipments = cus_shipments(request.user.username)
+
+    if request.method == 'GET' and 'tracking_number' in request.GET:
+        tracking_number = request.GET.get('tracking_number')
+        tracking_numbers = [shipment['tracking_number'] for shipment in shipments]
+        if tracking_number in tracking_numbers:
+            return redirect('tracking_page', tracking_number=tracking_number)
+        else:
+            messages.error(request, "Invalid Tracking Number.")
+            return redirect('customer_shipments')
+
+    
     context = {
-        "username": request.user.username ,
+        "username": request.user.username,
         "shipments": shipments
     }
-    print(shipments)
-    
     return render(request, "customer_shipment.html", context)
+
 
 def tracking_page(request, tracking_number):
     return render(request, "customer_tracking.html", {"tracking_number": tracking_number})
