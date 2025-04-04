bde9f5341f577091fb25694a273658c350588522
Fazil Metharsha Y <fazilmetharsha@gmail.com>
2025-04-04 14:41:09 +0530

    modified:   SCM_webapp/SCM_webapp/__pycache__/urls.cpython-312.pyc 	modified:   SCM_webapp/customer/__pycache__/query.cpython-312.pyc 	modified:   SCM_webapp/customer/__pycache__/urls.cpython-312.pyc 	new file:   SCM_webapp/customer/db.py 	modified:   SCM_webapp/customer/urls.py 	modified:   SCM_webapp/login/__pycache__/urls.cpython-312.pyc 	new file:   SCM_webapp/manager/__init__.py 	new file:   SCM_webapp/manager/admin.py 	new file:   SCM_webapp/manager/apps.py 	new file:   SCM_webapp/manager/migrations/__init__.py 	new file:   SCM_webapp/manager/models.py 	new file:   SCM_webapp/manager/tests.py 	new file:   SCM_webapp/manager/views.py 	new file:   SCM_webapp/supplier/__init__.py 	new file:   SCM_webapp/supplier/admin.py 	new file:   SCM_webapp/supplier/apps.py 	new file:   SCM_webapp/supplier/migrations/__init__.py 	new file:   SCM_webapp/supplier/models.py 	new file:   SCM_webapp/supplier/tests.py 	new file:   SCM_webapp/supplier/views.py

--- SCM_webapp/customer/urls.py
+++ SCM_webapp/customer/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('',views.customer_home,name='customer_home'),
     path('shipments', views.customer_shipments, name='customer_shipments'),
-    path('tracking\<str:tracking_number>', views.tracking_page, name='tracking_page'),
+    path('tracking/<str:tracking_number>/', views.tracking_page, name='tracking_page'),
 ]
