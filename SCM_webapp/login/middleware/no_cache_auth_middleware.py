10c731c81b5dc67ba427177cc06d8a9671d10f38
Fazil Metharsha Y <fazilmetharsha@gmail.com>
2025-04-05 14:52:38 +0530

    m

---
+++ SCM_webapp/login/middleware/no_cache_auth_middleware.py
@@ -0,0 +1,16 @@
+from django.shortcuts import redirect
+from django.utils.deprecation import MiddlewareMixin
+
+class NoCacheAndAuthMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        protected_paths = ['/customer/', '/manager/']
+        for path in protected_paths:
+            if request.path.startswith(path) and not request.user.is_authenticated:
+                return redirect('/')
+
+    def process_response(self, request, response):
+        # Force no browser cache
+        response['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+        response['Pragma'] = 'no-cache'
+        response['Expires'] = '0'
+        return response
