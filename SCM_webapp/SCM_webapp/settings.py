26c4128a499bd43663e9ea13853e42c8a1db49a4
Fazil Metharsha Y <fazilmetharsha@gmail.com>
2025-04-05 14:48:57 +0530

    m

--- SCM_webapp/SCM_webapp/settings.py
+++ SCM_webapp/SCM_webapp/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'login.middleware.no_cache_auth_middleware.NoCacheAndAuthMiddleware',
 ]
 
 ROOT_URLCONF = 'SCM_webapp.urls'
