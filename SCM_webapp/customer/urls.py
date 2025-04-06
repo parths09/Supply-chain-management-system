db18c9200d218d9d77449a0a35efe088dbe40c25
parths09 <parthshelar80@gmail.com>
2025-04-06 11:58:16 +0530

    Update urls.py

--- SCM_webapp/customer/urls.py
+++ SCM_webapp/customer/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.customer_home,name='customer_home'),
-    path('orders', views.customer_orders, name='customer_orders'),
+    # path('',views.customer_home,name='customer_home'),
+    path('', views.customer_orders, name='customer_orders'),
     path('tracking-<str:tracking_number>', views.tracking_page, name='tracking_page'),
 ]
