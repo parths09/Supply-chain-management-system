1b39dd795dc66507bce29fd5c050d4d4d2c2d15d
parths09 <parthshelar80@gmail.com>
2025-04-12 15:21:45 +0530

    Manager updates

--- SCM_webapp/manager/views.py
+++ SCM_webapp/manager/views.py
@@ -14,7 +14,24 @@ def manager_shipments(request):
     if request.method=='POST':
         return render(request,'manager_shipments.html',{'username': request.user.username})
     else:
-        return render(request,'manager_shipments.html',{'username': request.user.username})
+        w_id=get_warehouse_id(request.user.username)
+        w_name = get_warehouse_name(w_id)
+        result = get_active_shipments(w_id)
+        active_shipments = []
+        for row in result:
+            shipments_dict = {}
+            shipments_dict['shipping_id'] = row.shp_id
+            shipments_dict['tracking_number'] = row.tracking_number
+            shipments_dict['move_to'] = get_warehouse_name(row.move_to) if row.move_to else 'Delivery location'
+            shipments_dict['shipping_status'] = row.shipping_status
+            shipments_dict['delivery_date'] = row.delivery_date
+            active_shipments.append(shipments_dict)
+        context = {
+            'username': request.user.username,
+            'active_shipments':active_shipments,
+            'warehouse_name':w_name,
+        }
+        return render(request,'manager_shipments.html',context)
 
 def manager_stocks(request):
     if request.method=='POST':
@@ -33,7 +50,16 @@ def manager_employees(request):
     if request.method=='POST':
         return render(request,'manager_employees.html',{'username': request.user.username})
     else:
-        return render(request,'manager_employees.html',{'username': request.user.username})
+        w_id=get_warehouse_id(request.user.username)
+        w_name = get_warehouse_name(w_id)
+        employees = get_employees(w_id)
+
+        context = {
+            'username': request.user.username,
+            'employees': employees,
+            'warehouse_name':w_name,
+        }
+        return render(request,'manager_employees.html',context)
 
 def manager_procurements(request):
     if request.method=='POST':
